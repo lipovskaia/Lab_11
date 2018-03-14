437b68834d86e79a793deda987e173af34fe2057
lipovskaia <natalia.lipovskaia@gmail.com>
2018-03-13 20:33:59 -0700

    lab 11

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/*eslint-env browser*/
+
+function init(){
+    "use strict";
+    var btnLogin = window.document.getElementById("login");
+    btnLogin.addEventListener("click", function () {
+      window.open("login.html", "login", "width=400, height = 400");  
+    });
+    
+    }
+
+window.addEventListener("load", init);
